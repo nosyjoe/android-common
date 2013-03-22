4e2f367d49f529cff6e67b477039a0bf57690e34
Philipp Engel <philipp@filzip.com>
2013-03-22 14:38:18 +0100

    added helper method to check if the debuggable flag is set

--- common-tools/src/main/java/com/nosyjoe/android/common/Tools.java
+++ common-tools/src/main/java/com/nosyjoe/android/common/Tools.java
@@ -1,12 +1,16 @@
 package com.nosyjoe.android.common;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 
 /**
  * @author Philipp Engel <philipp@filzip.com>
  */
 public class Tools {
-    
+
     private static float scale = -1;
 
     public static int toDDP(int dip) {
@@ -15,6 +19,25 @@ public class Tools {
         }
 
         return (int) (dip * scale + 0.5f);
+    }
+
+    public static boolean isDebuggable(Context context) {
+        boolean debug = false;
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(),
+                    PackageManager.GET_CONFIGURATIONS);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo != null) {
+            int flags = packageInfo.applicationInfo.flags;
+            if ((flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+                debug = true;
+            } else
+                debug = false;
+        }
+        return debug;
     }
 
 }
